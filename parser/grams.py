41caaa86fbf1c957f871e6e74e4d64d834ac1323
pecanoro <pecanoro@umich.edu>
2014-04-09 00:00:03 -0400

    Parser - Fixed Attributes

--- parser/grams.py
+++ parser/grams.py
@@ -31,12 +31,39 @@ def transform(attribute):
 	
 	newfile.write(",".join(str(x) for x in aux) + '\n')
 	
-	index = header[attribute]
+	att = header[attribute]
+	
+	arrest = header['Arrest']
+	domestic = header['Domestic']
+	beat = header['Beat']
+	district = header['District']
+	ward = header['Ward']
+	area = header['Community Area']
+	fbi = header['FBI Code']
+	lat = header['Latitude']
+	long = header['Longitude']
+	xcoor = header['X Coordinate']
+	ycoor = header['Y Coordinate']
+
+	index = []
+	index.append(att)
+	
+	index.append(arrest)
+	index.append(domestic)
+	index.append(beat)
+	index.append(district)
+	index.append(ward)
+	index.append(area)
+	index.append(fbi)
+	index.append(lat)
+	index.append(long)
+	index.append(xcoor)
+	index.append(ycoor)
 	
 	for l in lines:
 		words = l
 		for w in range(0,len(words)):
-			if(w != index):
+			if(w not in index):
 				sub = re.sub('[\;\,\?\!\(\)\"]',' ', words[w])
 				s = sub.split(' ')
 				for a in s:
@@ -44,6 +71,7 @@ def transform(attribute):
 						dict[a] = dict[a] + 1
 					else:
 						dict[a] = 1
+			
 						
 	myfile.seek(0)
 	trash = next(lines)
@@ -53,7 +81,7 @@ def transform(attribute):
 		words = l
 		for w in range(0,len(words)):
 			total = 0
-			if(w != index):
+			if(w not in index):
 				if (words[w] == ''):
 					result.append('None')
 				else:
@@ -63,8 +91,17 @@ def transform(attribute):
 						total = total + dict[a]
 						
 					result.append(str(total))
+					
+			elif(w == arrest or w == domestic):
+				if(str(words[w]) == 'false'):
+					result.append(str(0))
+				else:
+					result.append(str(1))
 			else:
-				result.append(str(words[w]))
+				if (words[w] == ''):
+					result.append('None')
+				else:
+					result.append(str(words[w]))
 				
 		newfile.write(",".join(result) + '\n')
 			
